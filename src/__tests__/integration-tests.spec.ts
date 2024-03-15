fda27faf181681850e828b810b2a734b0808fd91
John Egan <john.egan@trivalence.com>
2024-03-14 19:40:24 -0500

    d

--- src/__tests__/integration-tests.spec.ts
+++ src/__tests__/integration-tests.spec.ts
@@ -7,129 +7,110 @@ import { MockInputOutput } from '../evaluator/input-output'
 import { Environment } from '../evaluator/environment'
 import { formatJson } from '../utils/json-utils'
 
-describe('Integration Tests', () => {
+describe('Integration tests', () => {
   const httpClient = new MockHttpClient()
   const io = new MockInputOutput()
   const environment = new Environment()
   const evaluator = new Evaluator(environment, httpClient, io)
+
   beforeEach(() => {
     io.writes = []
     httpClient.sentRequests = []
   })
-  test('should send GET request', async () => {
-    httpClient.status = 200
-    httpClient.headers = { 'content-type': 'application/json' }
-    httpClient.body = { message: 'Hello' }
-    const input = `
-        GET https://api.example.com/items
-        content-type: application/json
-      `
-    const lexer = new Lexer(input)
-    const parser = new Parser(lexer)
-    const program = parser.parseProgram()
-    await evaluator.evaluate(program)
-    expect(httpClient.sentRequests.length).toBe(1)
-  })
-  test('should send GET request with the entire URl as a variable', async () => {
-    httpClient.status = 200
-    httpClient.headers = { 'content-type': 'application/json' }
-    httpClient.body = { message: 'Hello' }
-    environment.variables.url = 'https://api.example.com/items'
-    const input = `
-        GET {{ url }}
-        content-type: application/json
-      `
-    const lexer = new Lexer(input)
-    const parser = new Parser(lexer)
-    const program = parser.parseProgram()
-    await evaluator.evaluate(program)
-    expect(httpClient.sentRequests.length).toBe(1)
-    expect(httpClient.sentRequests[0].url).toBe('https://api.example.com/items')
-  })
-  test('should send GET request with query params', async () => {
-    httpClient.status = 200
-    httpClient.headers = { 'content-type': 'application/json' }
-    httpClient.body = { message: 'Hello' }
-    const input = `
-        GET https://api.example.com/users/search
-        &q=My search terms
-        &size=10
-        content-type: application/json
-      `
-    const lexer = new Lexer(input)
-    const parser = new Parser(lexer)
-    const program = parser.parseProgram()
-    await evaluator.evaluate(program)
-    expect(httpClient.sentRequests.length).toBe(1)
-    expect(httpClient.sentRequests[0].url).toBe('https://api.example.com/users/search?q=My+search+terms&size=10')
-  })
-  test('should send GET request with duplicate query params', async () => {
-    httpClient.status = 200
-    httpClient.headers = { 'content-type': 'application/json' }
-    httpClient.body = { message: 'Hello' }
-    const input = `
-        GET https://api.example.com/users/search
-        &column=Name
-        &column=Type
-        &column=Created Date
-        content-type: application/json
-      `
-    const lexer = new Lexer(input)
-    const parser = new Parser(lexer)
-    const program = parser.parseProgram()
-    await evaluator.evaluate(program)
-    expect(httpClient.sentRequests.length).toBe(1)
-    expect(httpClient.sentRequests[0].url).toBe(
-      'https://api.example.com/users/search?column=Name&column=Type&column=Created+Date'
-    )
-  })
-  test('should send POST request', async () => {
-    httpClient.status = 200
-    httpClient.headers = { 'content-type': 'application/json' }
-    httpClient.body = { message: 'Hello' }
-    const input = `
-        POST https://api.example.com/items
-        content-type: application/json
-        { "description": "Item desc", "cataloNumber": "123" }
-      `
-    const lexer = new Lexer(input)
-    const parser = new Parser(lexer)
-    const program = parser.parseProgram()
-    await evaluator.evaluate(program)
-    expect(httpClient.sentRequests.length).toBe(1)
-    expect(httpClient.sentRequests[0].body).toBe(`{ "description": "Item desc", "cataloNumber": "123" }`)
-  })
-  test('should print newline', async () => {
-    const input = `PRINT`
-    const lexer = new Lexer(input)
-    const parser = new Parser(lexer)
-    const program = parser.parseProgram()
-    await evaluator.evaluate(program)
-    expect(io.writes.length).toBe(1)
-    expect(io.writes[0]).toBe('')
-  })
-  test('should set variable and print it', async () => {
-    const input = `
-        SET foo = bar
 
-        PRINT {{foo}}
-      `
-    const lexer = new Lexer(input)
-    const parser = new Parser(lexer)
-    const program = parser.parseProgram()
-    await evaluator.evaluate(program)
-    expect(io.writes.length).toBe(1)
-    expect(io.writes[0]).toBe('bar')
-  })
-  test('should send POST request with array mapped variable', async () => {
-    httpClient.status = 200
-    httpClient.headers = { 'content-type': 'application/json' }
-    httpClient.body = { message: 'Hello' }
-    environment.variables.roles = [
-      { id: 1, name: 'Manager' },
-      { id: 2, name: 'Admin' }
-    ]
-    const input = `
+  describe('HTTP requests', () => {
+    test('should send GET request', async () => {
+      httpClient.status = 200
+      httpClient.headers = { 'content-type': 'application/json' }
+      httpClient.body = { message: 'Hello' }
+      const input = `
+          GET https://api.example.com/items
+          content-type: application/json
+        `
+      const lexer = new Lexer(input)
+      const parser = new Parser(lexer)
+      const program = parser.parseProgram()
+      await evaluator.evaluate(program)
+      expect(httpClient.sentRequests.length).toBe(1)
+    })
+    test('should send GET request with the entire URl as a variable', async () => {
+      httpClient.status = 200
+      httpClient.headers = { 'content-type': 'application/json' }
+      httpClient.body = { message: 'Hello' }
+      environment.variables.url = 'https://api.example.com/items'
+      const input = `
+          GET {{ url }}
+          content-type: application/json
+        `
+      const lexer = new Lexer(input)
+      const parser = new Parser(lexer)
+      const program = parser.parseProgram()
+      await evaluator.evaluate(program)
+      expect(httpClient.sentRequests.length).toBe(1)
+      expect(httpClient.sentRequests[0].url).toBe('https://api.example.com/items')
+    })
+    test('should send GET request with query params', async () => {
+      httpClient.status = 200
+      httpClient.headers = { 'content-type': 'application/json' }
+      httpClient.body = { message: 'Hello' }
+      const input = `
+          GET https://api.example.com/users/search
+          &q=My search terms
+          &size=10
+          content-type: application/json
+        `
+      const lexer = new Lexer(input)
+      const parser = new Parser(lexer)
+      const program = parser.parseProgram()
+      await evaluator.evaluate(program)
+      expect(httpClient.sentRequests.length).toBe(1)
+      expect(httpClient.sentRequests[0].url).toBe('https://api.example.com/users/search?q=My+search+terms&size=10')
+    })
+    test('should send GET request with duplicate query params', async () => {
+      httpClient.status = 200
+      httpClient.headers = { 'content-type': 'application/json' }
+      httpClient.body = { message: 'Hello' }
+      const input = `
+          GET https://api.example.com/users/search
+          &column=Name
+          &column=Type
+          &column=Created Date
+          content-type: application/json
+        `
+      const lexer = new Lexer(input)
+      const parser = new Parser(lexer)
+      const program = parser.parseProgram()
+      await evaluator.evaluate(program)
+      expect(httpClient.sentRequests.length).toBe(1)
+      expect(httpClient.sentRequests[0].url).toBe(
+        'https://api.example.com/users/search?column=Name&column=Type&column=Created+Date'
+      )
+    })
+    test('should send POST request', async () => {
+      httpClient.status = 200
+      httpClient.headers = { 'content-type': 'application/json' }
+      httpClient.body = { message: 'Hello' }
+      const input = `
+          POST https://api.example.com/items
+          content-type: application/json
+          { "description": "Item desc", "cataloNumber": "123" }
+        `
+      const lexer = new Lexer(input)
+      const parser = new Parser(lexer)
+      const program = parser.parseProgram()
+      await evaluator.evaluate(program)
+      expect(httpClient.sentRequests.length).toBe(1)
+      expect(httpClient.sentRequests[0].body).toBe(`{ "description": "Item desc", "cataloNumber": "123" }`)
+    })
+    test('should send POST request with array mapped variable', async () => {
+      httpClient.status = 200
+      httpClient.headers = { 'content-type': 'application/json' }
+      httpClient.body = { message: 'Hello' }
+      environment.variables.roles = [
+        { id: 1, name: 'Manager' },
+        { id: 2, name: 'Admin' }
+      ]
+      const input = `
         POST https://api.example.com/users
         content-type: application/json
         {
@@ -137,14 +118,41 @@ describe('Integration Tests', () => {
           "roleIds": {{ roles.map(role => role.id) }}
         }
       `
-    const lexer = new Lexer(input)
-    const parser = new Parser(lexer)
-    const program = parser.parseProgram()
-    await evaluator.evaluate(program)
-    expect(httpClient.sentRequests.length).toBe(1)
-    expect(JSON.parse(httpClient.sentRequests[0].body as string)).toEqual({
-      username: 'user1',
-      roleIds: [1, 2]
+      const lexer = new Lexer(input)
+      const parser = new Parser(lexer)
+      const program = parser.parseProgram()
+      await evaluator.evaluate(program)
+      expect(httpClient.sentRequests.length).toBe(1)
+      expect(JSON.parse(httpClient.sentRequests[0].body as string)).toEqual({
+        username: 'user1',
+        roleIds: [1, 2]
+      })
     })
   })
+
+  describe('Printing', () => {
+    test('should print newline', async () => {
+      const input = `PRINT`
+      const lexer = new Lexer(input)
+      const parser = new Parser(lexer)
+      const program = parser.parseProgram()
+      await evaluator.evaluate(program)
+      expect(io.writes.length).toBe(1)
+      expect(io.writes[0]).toBe('')
+    })
+    test('should set variable and print it', async () => {
+      const input = `
+          SET foo = bar
+  
+          PRINT {{foo}}
+        `
+      const lexer = new Lexer(input)
+      const parser = new Parser(lexer)
+      const program = parser.parseProgram()
+      await evaluator.evaluate(program)
+      expect(io.writes.length).toBe(1)
+      expect(io.writes[0]).toBe('bar')
+    })
+  })
+  
 })
